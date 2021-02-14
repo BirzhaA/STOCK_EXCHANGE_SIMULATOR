b554e7566108bedb7f68d30caf76cdd5871700ce
Nokitka <kolomiec_ns_0919@1511.ru>
2021-02-14 17:20:55 +0300

    new update

--- Birzha/app/src/main/java/com/example/birzha/Birzha_main.kt
+++ Birzha/app/src/main/java/com/example/birzha/Birzha_main.kt
@@ -53,7 +53,7 @@ suspend fun addDot(){
 }
 
 fun makeMainUser(): Person {
-    val x = (Random.nextDouble(10.0, 15.0)).toFloat()
+    //val x = (Random.nextDouble(10.0, 15.0)).toFloat()
     //val money =  ( (1.2361 * x*x*x - 0.6961 * x*x  + 0.4591 * x - 0.0055)  * 1.1).toInt()
     val money = 1000
     return Person (
